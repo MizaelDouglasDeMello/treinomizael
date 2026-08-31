e23feb8b0000975824c65fc9c844a2d37859ad34
Mizael Douglas <98599652+MizaelDouglasDeMello@users.noreply.github.com>
2026-08-30 23:15:39 -0300

    Test
    Test

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE = 'treino-v1';
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(['./', './index.html'])).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+// Cache primeiro: abre mesmo sem sinal na academia.
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then(hit => hit || fetch(e.request).then(res => {
+      if (res && res.status === 200 && res.type === 'basic') {
+        const copia = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, copia));
+      }
+      return res;
+    }))
+  );
+});
